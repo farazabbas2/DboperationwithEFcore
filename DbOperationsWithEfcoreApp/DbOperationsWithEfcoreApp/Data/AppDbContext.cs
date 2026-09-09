35b8b0d0303580a490803285d67a4d41e9947d52
CT-Musawwir <CT-Musawwir@ERAIT1104>
2026-09-09 16:02:13 +0530

    Initial ASP.NET Core project

---
+++ DbOperationsWithEfcoreApp/DbOperationsWithEfcoreApp/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DbOperationsWithEfcoreApp.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+
+    }
+}
